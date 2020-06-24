92f3b07d16509f219d643d7f269462c2c10a4f24
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-06-23 21:07:28 -0500

    working on pool issues

--- controllers/employees.js
+++ controllers/employees.js
@@ -2,18 +2,29 @@ const mysql = require('mysql')
 const pool = require('../mysql/connections')
 const { error500, error404 } = require('../mysql/error')
 
-const getEmployees = (req, res) => { res.send('getting employees...')}
+//gets
+const getEmployees = (req, res) => { 
+  //select all the employees 
+  pool.query("SELECT * FROM employees", (err, row) => {
+    if(err) return error404(res, err)
+    if(err) return error500(res, err)
+    return res.json(rows)
+  })
+}
 
 const getEmployeesById = (req, res) => { res.send('getting employees...')}
 
 const getEmployeesByFirstName = (req, res) => { res.send('getting employees...')}
 
+//puts 
 
-pool.query(sql, (err, results) => {
-  if (err) return error500(res, err)
-  if (err) return error404(res, err)
-  return res.json({ err});
-})
+//error handling
+
+// pool.query(mysql, (err, results) => {
+//   if (err) return error500(res, err)
+//   if (err) return error404(res, err)
+//   return res.json({ err});
+// })
 
 module.exports = { 
   getEmployees, 
