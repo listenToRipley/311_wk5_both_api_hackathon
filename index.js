cb0b568b0a47debdd3122bfecd980eb9aa8c9e84
MrGRivera <gabrielrivera2889@gmail.com>
2020-06-25 20:48:19 -0500

    imported salaries routes and controller

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4001;
 const employeesRouter = require('./routes/employees')
+const salariesRouter = require('./routes/salaries')
 
 const bodyParser = require("body-parser");
 const e = require("express");
 
 app.use(bodyParser.json())
 app.use('/employees', employeesRouter)
+app.use('/salaries', salariesRouter)
 
 app.get('/', (req, res) => {
   res.send('Welcome to our API!')
--- routes/salaries.js
+++ routes/salaries.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const salariesController = require("../controllers/salaries");
 
 //GETS
-router.get("/salaries", salariesController.getCurrentSalaries);
+router.get("/employees", salariesController.getCurrentSalaries);
 
-router.get("/salary/:emp_no", salariesController.getSalaryByEmpNo);
+router.get("/:id", salariesController.getSalaryByEmpNo);
 
-router.get("/salary/:first_name", salariesController.getSalaryByFirstName);
+router.get("/firstname/:first_name", salariesController.getSalaryByFirstName);
 
 
-modules.export = router;
+module.exports = router;
